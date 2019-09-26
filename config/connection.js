e130ee14d427b0e4889bb73a1bdb639acdd64b99
Syed T. Akbar <sakbar01@icloud.com>
2019-09-25 21:36:00 -0500

    adding heroku cleardb config

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,20 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
+const localconnection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "test",
   database: "burgers_db"
+});
+
+const connection = mysql.createConnection({
+  host: "us-cdbr-iron-east-02.cleardb.net",
+  port: 3306,
+  user: "bbb262ca1e5e65",
+  password: "b9fc5273",
+  database: "heroku_295aff39c7aec8e"
 });
 
 // Make connection.
